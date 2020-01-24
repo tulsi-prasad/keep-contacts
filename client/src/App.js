0bbe5e7e9d95bc5e0aba775b7dd36bcf616c872c
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-01-24 17:02:36 +0530

    contacts context and global state

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,23 @@ import Navbar from "../src/components/layouts/Navbar";
 import Home from "../src/components/pages/Home";
 import About from "../src/components/pages/About";
 
+import ContactState from "../src/context/contact/ContactState";
+
 function App() {
     return (
-        <Router>
-            <Fragment>
-                <Navbar />
-                <div className="container">
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/about" component={About} />
-                    </Switch>
-                </div>
-            </Fragment>
-        </Router>
+        <ContactState>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/about" component={About} />
+                        </Switch>
+                    </div>
+                </Fragment>
+            </Router>
+        </ContactState>
     );
 }
 
