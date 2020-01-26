a9a09b7bb9786330881e76f832ce7ee04d3f5018
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-01-26 18:05:33 +0530

    update contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -95,8 +95,25 @@ const ContactState = props => {
     };
 
     // Update contact
-    const updateContact = contact => {
-        dispatch({ type: UPDATE_CONTACT, payload: contact });
+    const updateContact = async contact => {
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+
+        try {
+            const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+            dispatch({
+                type: UPDATE_CONTACT,
+                payload: res.data
+            });
+        } catch (err) {
+            dispatch({
+                type: CONTACT_ERROR,
+                payload: err.response.msg
+            });
+        }
     };
 
     // Filter contact
