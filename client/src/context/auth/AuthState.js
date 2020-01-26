d28a55643181015a674cc25a725a9452c9c820a6
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-01-26 16:00:12 +0530

    user login

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -8,7 +8,9 @@ import {
     REGISTER_FAIL,
     CLEAR_ERRORS,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from "../types";
 
 const AuthState = props => {
@@ -69,7 +71,29 @@ const AuthState = props => {
     };
 
     // Login user
-    const login = () => console.log("login");
+    const login = async formData => {
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        };
+
+        try {
+            const res = await axios.post("/api/auth", formData, config);
+
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            });
+
+            loadUser();
+        } catch (err) {
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: err.response.data.msg
+            });
+        }
+    };
 
     // Logout
     const logout = () => console.log("logout");
--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -3,7 +3,9 @@ import {
     REGISTER_FAIL,
     CLEAR_ERRORS,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from "../types";
 
 export default (state, action) => {
@@ -16,6 +18,7 @@ export default (state, action) => {
                 user: action.payload
             };
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem("token", action.payload.token);
             return {
                 ...state,
@@ -25,6 +28,7 @@ export default (state, action) => {
             };
         case REGISTER_FAIL:
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem("token");
             return {
                 ...state,
